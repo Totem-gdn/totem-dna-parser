b4610b3893e4b193ae7cbbeb72d1a4885eb9eb89
Denis.S <denis.s@MacBook-Pro-3.local>
2023-02-01 21:52:18 +0200

    replace hex parser

--- index.js
+++ index.js
@@ -102,38 +102,25 @@ class DNAParser {
     return id % 100
   }
 
-  _parseHexString (str) {
-    const lookup = {
-      '0': '0000',
-      '1': '0001',
-      '2': '0010',
-      '3': '0011',
-      '4': '0100',
-      '5': '0101',
-      '6': '0110',
-      '7': '0111',
-      '8': '1000',
-      '9': '1001',
-      'a': '1010',
-      'b': '1011',
-      'c': '1100',
-      'd': '1101',
-      'e': '1110',
-      'f': '1111',
-      'A': '1010',
-      'B': '1011',
-      'C': '1100',
-      'D': '1101',
-      'E': '1110',
-      'F': '1111'
-    };
-    let ret = '';
-    for (let i = 0, len = str.length; i < len; i++) {
-      if (lookup[str[i]]) {
-        ret += ((ret.length === 0 && lookup[str[i]] === '0000') ? '' : lookup[str[i]]);
-      }
-    }
-    return ret;
+  _parseHexString (hexStr) {
+       // Declare a lookup table for hexadecimal values 
+    let hexTable = { 
+        '0': '0000', '1': '0001', '2': '0010', '3': '0011', 
+        '4': '0100', '5': '0101', '6': '0110', '7': '0111', 
+        '8': '1000', '9': '1001', 'a': '1010', 'b': '1011', 
+        'c': '1100', 'd': '1101', 'e': '1110', 'f': '1111' 
+    } 
+  
+    // Initialize a variable to store binary string 
+    let binStr = ''; 
+  
+    // Convert each hexadecimal character 
+    // to its binary equivalent 
+    for (let i = 0; i < hexStr.length; i++) 
+        binStr += hexTable[hexStr[i]]; 
+  
+    // Return the converted binary string 
+    return binStr; 
   }
 }
 
